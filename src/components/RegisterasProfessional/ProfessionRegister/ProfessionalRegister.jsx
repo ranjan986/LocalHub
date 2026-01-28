6b27b8d8578e6c234e67e896140bb004cc648f1f
Ranjan Kushwaha <165882508+ranjan986@users.noreply.github.com>
2026-01-28 15:57:10 +0530

    Update API endpoint for professional registration

--- src/components/RegisterasProfessional/ProfessionRegister/ProfessionalRegister.jsx
+++ src/components/RegisterasProfessional/ProfessionRegister/ProfessionalRegister.jsx
@@ -34,7 +34,7 @@ function ProfessionalRegister() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const response = await fetch(
-            'http://localhost:5000/api/handyman/signup',
+            'https://local-hub-backend.onrender.com/api/handyman/signup',
             {
                 method: "POST",
                 headers: {
